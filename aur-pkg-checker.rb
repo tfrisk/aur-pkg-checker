0415e61953ada43e3d1edd959abb0045eaa8976b
Teemu Frisk <teemu.frisk@gmail.com>
2015-02-03 13:55:57 +0200

    First crude test version

---
+++ aur-pkg-checker.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/env ruby
+#
+# Arch Linux user repository version checking tool
+# This is a simple tool that just checks if you have all the
+# latest packages from AUR. See https://aur.archlinux.org/
+# for more information about AUR.
+#
+# (C) 2015 Teemu Frisk
+# Distributed under MIT License
+
+require 'net/http' # use net/http because it's in ruby std libs
+
+# list of installed packages
+# hash with pkg names as keys and versions as values
+# TODO: get the actual list from pacman (Arch Linux package manager)
+installed_pkg_list = Hash[
+  "blueman"                 => "1.99.alpha1-2",
+  "sublime-text"            => "2.0.2-3",
+]
+
+# PKGBUILD file path example:
+# https://aur.archlinux.org/packages/bl/blueman/PKGBUILD
+# note the extra directory of first 2 chars from the name
+def get_pkg_info(pkgname)
+  Net::HTTP.get(
+    URI.parse(
+      "https://aur.archlinux.org/packages/" + pkgname)) #+
+      #pkgname[0..1] + "/" +
+      #pkgname +
+      #{}"/PKGBUILD"))
+end
+
+# version line example:
+# <h2>Package Details: blueman 1.99.alpha1-2</h2>
+# this current implementation is probably too fragile for production use
+def get_latest_pkg_version(infocontent)
+  # first get the relevant file with regex
+  versionline = /^.*Package Details:.*$/.match(infocontent)[0]
+  # then use split to extract the version info
+  versionline.split(/[\s,<]/)[5]
+end
+
+#print get_pkg_pkgbuild("blueman")
+#print get_latest_pkg_version(get_pkg_info("sublime-text"))
+
+print "Checking latest versions\n"
+installed_pkg_list.each do |name, current_version|
+  latest_version = get_latest_pkg_version(get_pkg_info(name))
+
+  print name + ": " + current_version
+  if current_version == latest_version
+    print " => up to date, OK\n"
+  else
+    print " => new version available: #{latest_version}\n"
+  end
+end

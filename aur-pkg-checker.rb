e7cccb1bf717d28a2d002918a78d6dae4a17a255
Teemu Frisk <teemu.frisk@gmail.com>
2015-02-06 12:01:32 +0200

    Refactor to use class

--- aur-pkg-checker.rb
+++ aur-pkg-checker.rb
@@ -10,48 +10,56 @@
 
 require 'net/http' # use net/http because it's in ruby std libs
 
-# list of installed packages
-# hash with pkg names as keys and versions as values
-def get_installed_pkg_list()
-  pkg_list = Hash.new() # init new hash
-  # run pacman command and add results to hash
-  pacman_output = %x[pacman -Qm].split(/[\n]/)
-  pacman_output.each do |pkg|
-    name,version = pkg.split
-    pkg_list[name] = version
+class AurPackageChecker
+  # list of installed packages
+  # hash with pkg names as keys and versions as values
+  def get_installed_pkg_list()
+    pkg_list = Hash.new() # init new hash
+    # run pacman command and add results to hash
+    pacman_output = %x[pacman -Qm].split(/[\n]/)
+    pacman_output.each do |pkg|
+      name,version = pkg.split
+      pkg_list[name] = version
+    end
+    pkg_list
   end
-  pkg_list
+
+  # get package info from its AUR page:
+  # https://aur.archlinux.org/packages/blueman/
+  def get_pkg_info(pkgname)
+    Net::HTTP.get(
+      URI.parse("https://aur.archlinux.org/packages/" + pkgname))
+  end
+
+  # parse html source, version line example:
+  # <h2>Package Details: blueman 1.99.alpha1-2</h2>
+  # this current implementation is probably too fragile for production use
+  def get_latest_pkg_version(infocontent)
+    # first get the relevant file with regex
+    versionline = /^.*Package Details:.*$/.match(infocontent)[0]
+    # then use split to extract the version info
+    versionline.split(/[\s,<]/)[5]
+  end
+
+  def compare_versions(current, latest)
+    %x[vercmp #{current} #{latest}].to_i
+  end
 end
+
+checker = AurPackageChecker.new
 
 # read installed packages
-installed_pkg_list = get_installed_pkg_list
-
-# get package info from its AUR page:
-# https://aur.archlinux.org/packages/blueman/
-def get_pkg_info(pkgname)
-  Net::HTTP.get(
-    URI.parse("https://aur.archlinux.org/packages/" + pkgname))
-end
-
-# parse html source, version line example:
-# <h2>Package Details: blueman 1.99.alpha1-2</h2>
-# this current implementation is probably too fragile for production use
-def get_latest_pkg_version(infocontent)
-  # first get the relevant file with regex
-  versionline = /^.*Package Details:.*$/.match(infocontent)[0]
-  # then use split to extract the version info
-  versionline.split(/[\s,<]/)[5]
-end
+installed_pkg_list = checker.get_installed_pkg_list
 
 print "Current time is #{Time.now}\n"
 print "Checking package versions\n"
 installed_pkg_list.each do |name, current_version|
-  latest_version = get_latest_pkg_version(get_pkg_info(name))
+  latest_version = checker.get_latest_pkg_version(checker.get_pkg_info(name))
 
   print name + ": " + current_version
 
   # use pacmans own vercmp tool to check the versions
-  cstatus = %x[vercmp #{current_version} #{latest_version}].to_i
+  cstatus = checker.compare_versions(current_version, latest_version)
   if cstatus == 0 # current = latest
     print " => OK\n"
   elsif cstatus < 0 # current < latest
